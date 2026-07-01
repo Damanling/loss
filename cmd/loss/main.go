933be648d1736bebba01999e21e10158c9d9bda2
Damanling <maxagurbek@gmail.com>
2026-07-01 11:11:20 +0300

    feat: Без аргументов usage + exit

--- cmd/loss/main.go
+++ cmd/loss/main.go
@@ -7,28 +7,28 @@ import (
 
 const version = "0.1"
 
- func main(){
-	if( len(os.Args) < 2){
-	usage()
-	os.Exit(1)
+func main() {
+	if len(os.Args) < 2 {
+		usage()
+		os.Exit(1)
 	}
 
 	if arg := os.Args[1]; arg == "hello" {
-		fmt.Println("hello, brother — это loss v"+ version)
+		fmt.Println("hello, brother — это loss v" + version)
 	} else if arg := os.Args[1]; arg == "echo" {
-			fmt.Println("echo: ", os.Args[2:])
-			if len(os.Args) < 3 {
-				usage()
-				os.Exit(1)
+		fmt.Println("echo: ", os.Args[2:])
+		if len(os.Args) < 3 {
+			usage()
+			os.Exit(1)
 		}
 	} else {
 		usage()
 		os.Exit(1)
 	}
- }
+}
 
- func usage() {
-	fmt.Fprintln(os.Stderr,"usage: ")
-	fmt.Fprintln(os.Stderr,"loss hello")
-	fmt.Fprintln(os.Stderr,"loss echo args")
- }
+func usage() {
+	fmt.Fprintln(os.Stderr, "usage: ")
+	fmt.Fprintln(os.Stderr, "loss hello")
+	fmt.Fprintln(os.Stderr, "loss echo args")
+}
