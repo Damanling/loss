dd62f3312fa1f3b573968c1a6b8a8c311ce5c988
Damanling <maxagurbek@gmail.com>
2026-07-01 11:18:56 +0300

    feat: Без аргументов → usage + exit 1 (#6)
    * feat: Без аргументов → usage + exit 1
    
    * feat: loss hello и эхо аргумента

--- cmd/loss/main.go
+++ cmd/loss/main.go
@@ -9,14 +9,26 @@ const version = "0.1"
 
 func main() {
 	if len(os.Args) < 2 {
-		return
+		usage()
+		os.Exit(1)
 	}
 
-	commandName := os.Args[1]
-	if commandName == "hello" {
-		fmt.Printf("hello, brother - это loss v%s\n", version)
-		return
+	if arg := os.Args[1]; arg == "hello" {
+		fmt.Println("hello, brother — это loss v" + version)
+	} else if arg := os.Args[1]; arg == "echo" {
+		fmt.Println("echo: ", os.Args[2:])
+		if len(os.Args) < 3 {
+			usage()
+			os.Exit(1)
+		}
+	} else {
+		usage()
+		os.Exit(1)
 	}
+}
 
-	fmt.Printf("got: %s\n", commandName)
+func usage() {
+	fmt.Fprintln(os.Stderr, "usage: ")
+	fmt.Fprintln(os.Stderr, "loss hello")
+	fmt.Fprintln(os.Stderr, "loss echo args")
 }
