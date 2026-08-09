3ad3d648af1c015fceee969fc812335dec552928
kayn <suvorovka04@gmail.com>
2026-08-09 13:20:44 +0300

    feat: flags commands loss hello

--- cmd/loss/main.go
+++ cmd/loss/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
@@ -12,21 +13,29 @@ func main() {
 		usage()
 		os.Exit(1)
 	}
+	hello := flag.NewFlagSet(os.Args[1], flag.ExitOnError)
 
-	if arg := os.Args[1]; arg == "hello" {
-		fmt.Println("hello, brother — это loss v" + version)
-	} else if arg := os.Args[1]; arg == "echo" {
-		fmt.Println("echo: ", os.Args[2:])
+	switch os.Args[1] {
+	case "hello":
+		var name = hello.String("name", "brother", "имя пользователя")
+		if err := hello.Parse(os.Args[2:]); err != nil {
+			usage()
+			os.Exit(1)
+		}
+		fmt.Println("hello, " + *name + " — это loss v" + version)
+
+	case "echo":
 		if len(os.Args) < 3 {
 			usage()
 			os.Exit(1)
 		}
-	} else {
+		fmt.Println("echo:", os.Args[2:])
+
+	default:
 		usage()
 		os.Exit(1)
 	}
 }
-
 func usage() {
 	fmt.Fprintln(os.Stderr, "usage: ")
 	fmt.Fprintln(os.Stderr, "loss hello")
