ba903204608c02ca46a7a7bae58243eaee329937
Dima <leonov@MacBook-Air-Dima.local>
2026-08-16 11:13:48 +0300

    refactor: после ревью

--- cmd/loss/main.go
+++ cmd/loss/main.go
@@ -13,29 +13,34 @@ func main() {
 		usage()
 		os.Exit(1)
 	}
+
 	hello := flag.NewFlagSet(os.Args[1], flag.ExitOnError)
 
 	switch os.Args[1] {
 	case "hello":
 		var name = hello.String("name", "brother", "имя пользователя")
+
 		if err := hello.Parse(os.Args[2:]); err != nil {
 			usage()
 			os.Exit(1)
 		}
-		fmt.Println("hello, " + *name + " — это loss v" + version)
+
+		fmt.Printf("hello, %s — это loss v%s \n", *name, version)
 
 	case "echo":
 		if len(os.Args) < 3 {
 			usage()
 			os.Exit(1)
 		}
-		fmt.Println("echo:", os.Args[2:])
+
+		fmt.Printf("echo: %s\n", os.Args[2:])
 
 	default:
 		usage()
 		os.Exit(1)
 	}
 }
+
 func usage() {
 	fmt.Fprintln(os.Stderr, "usage: ")
 	fmt.Fprintln(os.Stderr, "loss hello")
