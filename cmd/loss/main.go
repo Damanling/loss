63e50d6fb98f34462875be09e62d044f4ac57d96
Damanling <maxagurbek@gmail.com>
2026-08-18 17:53:26 +0300

    feat: flags commands loss hello (#8)
    * feat: flags commands loss hello
    
    * refactor: после ревью
    
    ---------
    
    Co-authored-by: kayn <suvorovka04@gmail.com>
    Co-authored-by: Dima <leonov@MacBook-Air-Dima.local>

--- cmd/loss/main.go
+++ cmd/loss/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
@@ -13,15 +14,28 @@ func main() {
 		os.Exit(1)
 	}
 
-	if arg := os.Args[1]; arg == "hello" {
-		fmt.Println("hello, brother — это loss v" + version)
-	} else if arg := os.Args[1]; arg == "echo" {
-		fmt.Println("echo: ", os.Args[2:])
+	hello := flag.NewFlagSet(os.Args[1], flag.ExitOnError)
+
+	switch os.Args[1] {
+	case "hello":
+		var name = hello.String("name", "brother", "имя пользователя")
+
+		if err := hello.Parse(os.Args[2:]); err != nil {
+			usage()
+			os.Exit(1)
+		}
+
+		fmt.Printf("hello, %s — это loss v%s \n", *name, version)
+
+	case "echo":
 		if len(os.Args) < 3 {
 			usage()
 			os.Exit(1)
 		}
-	} else {
+
+		fmt.Printf("echo: %s\n", os.Args[2:])
+
+	default:
 		usage()
 		os.Exit(1)
 	}
