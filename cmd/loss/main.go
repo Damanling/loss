05b9f56cd3698b7292a013bf27aadd060460a54f
Dima <leonov@MacBook-Air-Dima.local>
2026-09-06 21:13:44 +0300

    fix: исправил категории и очистил код

--- cmd/loss/main.go
+++ cmd/loss/main.go
@@ -4,25 +4,17 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"slices"
 )
 
 const version = "0.1"
 
 func list() {
-	fmt.Println("1500")
-	fmt.Println("food")
-	fmt.Println("кофе")
+	
 }
+var categories = []string{"food", "transport", "fun", "bills", "other"}
 
-type Categories string
-
-const (
-	CategoryFood      = "food"
-	CategoryTransport = "transport"
-	CategoryFun       = "fun"
-	CategoryBills     = "bills"
-	CategoryOther     = "other"
-)
+func validCategory(c string) bool { return slices.Contains(categories, c) }
 
 func main() {
 	if len(os.Args) < 2 {
@@ -31,14 +23,12 @@ func main() {
 	}
 
 	hello := flag.NewFlagSet(os.Args[1], flag.ExitOnError)
-	add := flag.NewFlagSet(os.Args[1], flag.ExitOnError)
-
+	
 	switch os.Args[1] {
 	case "hello":
 		var name = hello.String("name", "brother", "имя пользователя")
 
 		if err := hello.Parse(os.Args[2:]); err != nil {
-			usage()
 			os.Exit(1)
 		}
 
@@ -53,7 +43,9 @@ func main() {
 		fmt.Printf("echo: %s\n", os.Args[2:])
 
 	case "add":
-		var amount = add.Int("amount", 0, "потраченная сумма")
+		add := flag.NewFlagSet("add", flag. ContinueOnError)
+
+		var amount = add.Int64("amount", 0, "потраченная сумма")
 		var cat = add.String("cat", "empty", "категории")
 		var note = add.String("note", "empty", "что купил")
 
@@ -72,30 +64,20 @@ func main() {
 			os.Exit(1)
 		}
 
-		switch *cat {
-		case CategoryFood, CategoryTransport, CategoryFun, CategoryBills, CategoryOther:
-			fmt.Println("Категория выбрана верно")
-		case "empty":
-			fmt.Fprintln(os.Stderr, "cat flag is required")
-			os.Exit(1)
-		default:
+		if !validCategory(*cat) {
 			fmt.Fprintln(os.Stderr, "unknown category, allowed: food, transport, fun, bills, other")
 			os.Exit(1)
 		}
 
-		fmt.Println(*amount)
-		fmt.Println(*cat)
-		fmt.Println(*note)
+		fmt.Printf("%d,%s,%s\n", *amount, *cat, *note)
 
 	case "list":
-		list()
+		listFlags := flag.NewFlagSet("list", flag.ContinueOnError)
+		if err := listFlags.Parse(os.Args[2:]); err != nil {
+			os.Exit(1)
+		}
 
-	case "cats":
-		fmt.Println(CategoryFood)
-		fmt.Println(CategoryTransport)
-		fmt.Println(CategoryFun)
-		fmt.Println(CategoryBills)
-		fmt.Println(CategoryOther)
+		list()
 
 	default:
 		usage()
@@ -109,5 +91,4 @@ func usage() {
 	fmt.Fprintln(os.Stderr, "loss echo args")
 	fmt.Fprintln(os.Stderr, "loss add --amount 100 --cat food --note 'что купил'")
 	fmt.Fprintln(os.Stderr, "loss list")
-	fmt.Fprintln(os.Stderr, "loss cats")
 }
